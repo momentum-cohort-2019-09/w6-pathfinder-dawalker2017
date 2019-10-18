c9483e80d18b1c25a242159a1b20a39ff2b534fc
danielwalker <dawalker2017@outlook.com>
2019-10-17 23:38:24 -0400

    10.17.19 11:40PM CHECKPOINT: beginning to write main Map into a Class

--- stone_mountain_pass.py
+++ stone_mountain_pass.py
@@ -1,8 +1,24 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFilter
 
 
-img1 = Image.new("RGBA", (601, 601), color=(65, 60, 35, 255))
-draw = ImageDraw.Draw(img1)
-img1.save('img1.png')
+# Creating a class "Map" that deals with the actual map display.
+class Map:
+    """
+    Class pertaining to the actual Map image.
+    Containing; image, coordinates, color, and assigning color to pixels.
+    """
+    with open("elevation_small.txt") as elevations_file:
+        file_contents = elevations_file.read()
+        # print(file_contents)
 
-with open()
+    def draw_map(self):
+        img = Image.new("RGBA", (601, 601), color=(65, 60, 35, 255))
+        draw = ImageDraw.Draw(img)
+        # draw.rectangle([0, 0, 600, 600], width=5, outline=(0, 0, 0, 255))
+        img.save('map.png')
+
+    # def list_all_coordinates
+
+    # def get_maximum_coordinate
+
+    # def get_minimum_coordinate
